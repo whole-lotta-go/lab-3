4c9df506164dc62f965c088ef49db7b89f72c5f0
Vitalii Chyhryn <chyhryn.vitalii@gmail.com>
2025-04-19 00:09:49 +0300

    Make move_diagonal bouncy

--- scripts/move_diagonal.sh
+++ scripts/move_diagonal.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-x=0.1
-y=0.1
+x=0.05
+y=0.05
+dx=0.005
+dy=0.005
 
-curl -X POST http://localhost:17000 -d "figure 0.1 0.1"
+curl -X POST http://localhost:17000 -d "figure 0 0"
 
-for i in {1..10}; do
+while true; do
     curl -X POST http://localhost:17000 -d "move $x $y"
     curl -X POST http://localhost:17000 -d "update"
 
-    x=$(echo "$x + 0.05" | bc)
-    y=$(echo "$y + 0.05" | bc)
+    x=$(echo "$x + $dx" | bc)
+    y=$(echo "$y + $dy" | bc)
 
-    sleep 1
+
+    if (( $(echo "$x >= 0.8" | bc -l) )); then
+        x=0.8
+        dx=$(echo "-($dx)" | bc -l)
+    elif (( $(echo "$x <= 0.2" | bc -l) )); then
+        x=0.2
+        dx=$(echo "-($dx)" | bc -l)
+    fi
+
+    if (( $(echo "$y >= 0.8" | bc -l) )); then
+        y=0.8
+        dy=$(echo "-($dy)" | bc -l)
+    elif (( $(echo "$y <= 0.2" | bc -l) )); then
+        y=0.2
+        dy=$(echo "-($dy)" | bc -l)
+    fi
 done
