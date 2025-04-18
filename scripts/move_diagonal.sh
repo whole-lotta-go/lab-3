9ea4dc7c4bd27606c242f989d6c5681f2e62d788
Vitalii Chyhryn <chyhryn.vitalii@gmail.com>
2025-04-18 23:55:55 +0300

    Fix scripts

--- scripts/move_diagonal.sh
+++ scripts/move_diagonal.sh
@@ -3,6 +3,8 @@
 x=0.1
 y=0.1
 
+curl -X POST http://localhost:17000 -d "figure 0.1 0.1"
+
 for i in {1..10}; do
     curl -X POST http://localhost:17000 -d "move $x $y"
     curl -X POST http://localhost:17000 -d "update"
