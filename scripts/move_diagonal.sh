0f5f861629f7e5c70040c2605c4fcb792c236963
Даник <pistriuga.danylo@lll.kpi.ua>
2025-04-18 23:49:27 +0300

    Add bash scripts

---
+++ scripts/move_diagonal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+x=0.1
+y=0.1
+
+for i in {1..10}; do
+    curl -X POST http://localhost:17000 -d "move $x $y"
+    curl -X POST http://localhost:17000 -d "update"
+
+    x=$(echo "$x + 0.05" | bc)
+    y=$(echo "$y + 0.05" | bc)
+
+    sleep 1
+done
---
+++ scripts/reset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -X POST http://localhost:17000 -d "reset"
+curl -X POST http://localhost:17000 -d "white"
+curl -X POST http://localhost:17000 -d "update"
